6936f6e289729e87e130938495eb1167685c90a4
savasda <sava2503@gmail.com>
2016-07-31 16:45:06 +0300

    3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,8 +1,12 @@
 'use strict';
 
-/*controllers*/
+/* Controllers */
 var evasApp = angular.module('evasApp', []);
 
-evasApp.controller('evasProgramsCtr', function($scope){
+
+evasApp.controller('evasProgramsCtr', function($scope, $http, $location){
   $scope.title = 'Программы';
+  $http.get('js/json/programs.json').success(function(data, status, headers, config){
+    $scope.phones = data;
+  });
 })
