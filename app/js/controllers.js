436d75d0d330d502e9f613838087c54ab99ed862
savasda <sava2503@gmail.com>
2016-08-02 21:32:09 +0300

    разлинковка

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,52 @@
 'use strict';
 
 /* Controllers */
-var evasApp = angular.module('evasApp', []);
+var evasApp = angular.module('evasApp', ['ngRoute']);
+evasApp.config(['$routeProvider', function($routeProvide){
+	$routeProvide
+	.when('/', {
+		templateUrl: 'tepmlates/home.html',
+		controller: 'homeContactsCtr'
+	})
+	.when('/about', {
+		templateUrl: 'tepmlates/about.html',
+		controller: 'evasAboutCtr'
+	})
+	.when('/programs', {
+		templateUrl: 'tepmlates/programs.html',
+		controller: 'evasProgramsCtr'
+	})
+	.when('/galary', {
+		templateUrl: 'tepmlates/galarey.html',
+		controller: 'evasGalaryCtr'
+	})
+	.when('/contacts', {
+		templateUrl: 'tepmlates/contacts.html',
+		controller: 'evasContactsCtr'
+	})
+	.otherwise('/', {
+		redirectTo: '/',
+	})
+}])
 
-evasApp.controller('evasProgramsCtr', function($scope, $http) {
+evasApp.controller('evasProgramsCtr', function($scope, $http, $location) {
 	$scope.title = 'Программы';
 	$http.get('js/json/programs.json').success(function(data, status, headers,  config){
 		$scope.programs = data;
 	});
+});
+
+
+evasApp.controller('homeContactsCtr', function($scope, $http, $location) {
+});
+evasApp.controller('evasHomeCtr', function($scope, $http, $location) {
+});
+
+evasApp.controller('evasGalaryCtr', function($scope, $http, $location) {
+});
+
+evasApp.controller('evasAboutCtr', function($scope, $http, $location) {
+});
+
+evasApp.controller('evasContactsCtr', function($scope, $http, $location) {
 });
