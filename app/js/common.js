26725991c2a49792270a7ba4402744140cd397eb
savasda <sava2503@gmail.com>
2016-08-07 12:34:40 +0300

    -много добавлено

--- app/js/common.js
+++ app/js/common.js
@@ -9,23 +9,7 @@ $(function() {
 
 	$('.material-design-hamburger__icon').click(mobMnu)
 
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-			}
-		}
-	});
+	
 
 	$('.bg-holder').parallaxScroll({
 	  friction: 0.3
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,6 +24,10 @@ evasApp.config(['$routeProvider', function($routeProvide){
 		templateUrl: 'tepmlates/contacts.html',
 		controller: 'evasContactsCtr'
 	})
+	.when('/js/json/:id', {
+		templateUrl: 'tepmlates/programs-datail.html',
+		controller: 'evasProgramsDatailCtr'
+	})
 	.otherwise('/', {
 		redirectTo: '/',
 	})
@@ -31,22 +35,35 @@ evasApp.config(['$routeProvider', function($routeProvide){
 
 evasApp.controller('evasProgramsCtr', function($scope, $http, $location) {
 	$scope.title = 'Программы';
+	$scope.review = 'Детский развивающий центр “Eva’s TinyLand” разработал ряд программ, которые помогут ребенку развиваться гармонично. Мы предлагаем различные направления в соответствии с возрастом ребенка. Здесь вы можете ознакомиться с программами нашего центра.'
 	$http.get('js/json/programs.json').success(function(data, status, headers,  config){
 		$scope.programs = data;
 	});
 });
 
+evasApp.controller('evasProgramsDatailCtr', function($scope, $http, $location, $routeParams) {
+	$scope.id = $routeParams.id;
+	var url = 'js/json/details/'+$routeParams.id+'.json';
+	$http.get(url).success(function(data){
+		$scope.program = data;
+	})
+});
 
 evasApp.controller('homeContactsCtr', function($scope, $http, $location) {
+	$scope.title = 'Контакты';
 });
+
 evasApp.controller('evasHomeCtr', function($scope, $http, $location) {
 });
 
 evasApp.controller('evasGalaryCtr', function($scope, $http, $location) {
+	$scope.title = 'Галерея';
 });
 
 evasApp.controller('evasAboutCtr', function($scope, $http, $location) {
+	$scope.title = 'О нас';
 });
 
 evasApp.controller('evasContactsCtr', function($scope, $http, $location) {
+	$scope.title = 'Контакты';
 });
